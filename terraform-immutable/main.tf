c5f5bdea94fad5fbbf21b35721598a75d2648d99
Chandra Ummadi <ummadi.chandra@gmail.com>
2022-01-06 10:04:30 +0530

    updated immutable approach catalogue,shipping,payment,user and front end components components

---
+++ terraform-immutable/main.tf
@@ -0,0 +1,18 @@
+module "shipping" {
+  source              = "git::https://github.com/chandraummadi/terraform-immutable.git//app-module"
+  INSTANCE_TYPE       = var.INSTANCE_TYPE
+  ENV                 = var.ENV
+  SPOT_INSTANCE_COUNT = var.SPOT_INSTANCE_COUNT
+  OD_INSTANCE_COUNT   = var.OD_INSTANCE_COUNT
+  COMPONENT           = "shipping"
+  PORT                = 8080
+  IS_PRIVATE_LB       = true
+  LB_RULE_PRIORITY    = 103
+  APP_VERSION         = var.APP_VERSION
+  NEXUS_USER          = var.NEXUS_USER
+  NEXUS_PASS          = var.NEXUS_PASS
+  min_size            = var.min_size
+  max_size            = var.max_size
+  desired_capacity    = var.desired_capacity
+}
+
